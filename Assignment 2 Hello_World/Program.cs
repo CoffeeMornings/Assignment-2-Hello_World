111d417fab4461a1bc10baa538b3c1816ef46338
CoffeeMornings <bruun1993@gmail.com>
2024-01-22 15:39:32 +0100

    Added another sentence.

--- Assignment 2 Hello_World/Program.cs
+++ Assignment 2 Hello_World/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace Assignment_2_Hello_World;
-
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Keeping track of changes!");
+        Console.WriteLine("Learning how Git works!");
     }
 }
